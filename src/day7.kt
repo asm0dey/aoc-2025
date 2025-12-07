5a63a5255747c7395b6651f4d92bfe8ebffe2248
asm0dey <pavel.finkelshtein@gmail.com>
2025-12-07 13:40:36 +0100

    Day 7 visualisation

--- src/day7.kt
+++ src/day7.kt
@@ -4,7 +4,7 @@ fun main() {
         val newBeams =
             beams.flatMap { beam -> if (this[depth][beam] == '^') listOf(beam - 1, beam + 1) else listOf(beam) }
         val splitCount = newBeams.size - beams.size
-        return this.countSplitters(newBeams.toSet(), depth + 1, currentSplitterCount + splitCount)
+        return countSplitters(newBeams.toSet(), depth + 1, currentSplitterCount + splitCount)
     }
 
     tailrec fun List<String>.countBeams(beams: Map<Int, Long>, depth: Int = 0): Long {
@@ -17,11 +17,10 @@ fun main() {
                 }
                 .groupBy(Pair<Int, Long>::first)
                 .mapValues { it.value.sumOf(Pair<Int, Long>::second) }
-        return this.countBeams(newBeams, depth + 1)
+        return countBeams(newBeams, depth + 1)
     }
 
     fun part1(input: List<String>) = input.countSplitters(setOf(input.first().indexOf('S')))
-
     fun part2(input: List<String>) = input.countBeams(mapOf(input.first().indexOf('S') to 1))
 
     println("Test input part 1 " + part1(readFile("day7_t1")))
